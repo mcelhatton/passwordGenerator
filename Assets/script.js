83765b418d7bf654b5a9bccb63984c758e44aa99
mcelhatton <colinm@mobiledealerdata.com>
2022-03-14 11:19:04 -0700

    saving progress

--- Assets/script.js
+++ Assets/script.js
@@ -3,12 +3,7 @@
 
 // series of questions, password length, special characters, upper and lower case?
 // questions / prompts
-var isUppercase = window.prompt("Use upper case in your password?");
-var isSpecialCharacter = window.prompt("Use special characters in your password?");
-var passwordLength = window.prompt("How many characters do you want in your password? Minimum length 8, Maximum length 128.");
-console.log(isUppercase);
-console.log(isSpecialCharacter);
-console.log(passwordLength);
+
 
 // password validation length, criteria 
 
@@ -26,6 +21,41 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// generatePassword function
+function generatePassword() {
+  var isUppercase = window.prompt("Use upper case in your password? Enter YES or NO.");
+  var isSpecialCharacter = window.prompt("Use special characters in your password? Enter YES or NO.");
+  var passwordLength = window.prompt("How many characters do you want in your password? Minimum length 8, Maximum length 128.");
+  console.log(isUppercase);
+  console.log(isSpecialCharacter);
+  console.log(passwordLength);
+
+  // validate isUppercase 
+  if (isUppercase === Yes || isUppercase === yes || isUppercase === YES) {
+    isUppercase = true;
+  } else if (isUppercase === NO || isUppercase === no || isUppercase === No) {
+    isUppercase = false;
+  } else {
+    var isUppercase = window.prompt("Use upper case in your password? Enter YES or NO.");
+  }
+  // validate isSpecialCharacter
+  if (isSpecialCharacter === Yes || isSpecialCharacter === yes || isSpecialCharacter === YES) {
+    isSpecialCharacter = true;
+  } else if (isSpecialCharacter === NO || isSpecialCharacter === no || isSpecialCharacter === No) {
+    isSpecialCharacter = false;
+  } else {
+    var isSpecialCharacter = window.prompt("Use special characters in your password? Enter YES or NO.");
+  }
+
+  // validate length
+  if (passwordLength.length >= 8 && passwordLength <= 129) {
+
+  } else {
+    window.prompt("Please enter a proper length for your password.  Minimum of 8 characters and Maximum of 128.");
+  }
+
+}
 
 // display the generated password
 
