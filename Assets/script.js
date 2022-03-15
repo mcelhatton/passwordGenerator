8176d770023d2234cb7298c97115bcaff665285e
mcelhatton <colinm@mobiledealerdata.com>
2022-03-15 15:01:31 -0500

    saving final product

--- Assets/script.js
+++ Assets/script.js
@@ -73,7 +73,7 @@ function generatePassword() {
   for (var i = 0; i < passwordLength; i++) {
     var randomNumber = Math.floor(Math.random() * passCharacters.length);
     password += passCharacters.substring(randomNumber, randomNumber +1);
-    console.log(password);
+    //console.log(password);
   }
 
   // verify password criteria matches users input
