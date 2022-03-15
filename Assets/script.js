cdcee6d1aa6a8c7e88f065a1c6c0a8f09697628f
mcelhatton <colinm@mobiledealerdata.com>
2022-03-14 21:50:24 -0500

    saving progress

--- Assets/script.js
+++ Assets/script.js
@@ -30,45 +30,35 @@ function generatePassword() {
   console.log(useUppercase);
   console.log(useSpecialCharacter);
   console.log(passwordLength);
+
+  while (passwordLength < 8) {
+      var passwordLength = window.prompt("Please enter a number between 8 and 128.");
+    }
+
   if (useUppercase) {
     var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   } else {
     uppercase = "";
   }
+
   if (useSpecialCharacter) {
     var specialCharacter = "!@#$%^&*()"
   } else {
     specialCharacter = "";
   }
+
   var characters = "0123456789abcdefghijklmnopqrstuvwxy";
   var passCharacters = uppercase + specialCharacter + characters;
   var password = "";
+
   for (var i = 0; i <= passwordLength; i++) {
     var randomNumber = Math.floor(Math.random() * passCharacters.length);
     password += passCharacters.substring(randomNumber, randomNumber +1);
     console.log(password);
   }
   return password;
-  // validate isUppercase 
-  
-
-  // validate isSpecialCharacter
-  if (useSpecialCharacter) {
-
-  }
-
-  // validate length
-  if (passwordLength.length >= 8 && passwordLength <= 128) {
-    
-  } else {
-    window.prompt("Please enter a proper length for your password.  Minimum of 8 characters and Maximum of 128.");
-  }
-
-  // password array builder
 
 }
-
-// display the generated password
 
 
 
