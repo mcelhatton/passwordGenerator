ea3a7cda8602317dd2b79763f0d65303728a14c4
mcelhatton <colinm@mobiledealerdata.com>
2022-03-15 10:57:00 -0500

    saving progress

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,3 @@
-// Assignment code here
-
-
-// series of questions, password length, special characters, upper and lower case?
-// questions / prompts
-
-
-// password validation length, criteria 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -25,33 +17,51 @@ generateBtn.addEventListener("click", writePassword);
 // generatePassword function
 function generatePassword() {
   var useUppercase = window.confirm("Use upper case in your password? OK = YES.");
+  var useLowercase = window.confirm("Use lower case in your password? OK = YES.");
+  var useNumeric = window.confirm("Use numbers in your password? OK = YES.");
   var useSpecialCharacter = window.confirm("Use special characters in your password? OK = YES.");
   var passwordLength = window.prompt("How many characters do you want in your password? Minimum length 8, Maximum length 128.");
   console.log(useUppercase);
+  console.log(useLowercase);
+  console.log(useNumeric);
   console.log(useSpecialCharacter);
   console.log(passwordLength);
 
+  while (useUppercase === false && useLowercase === false && useNumeric === false) {
+      var useUppercase = window.confirm("Use upper case in your password? OK = YES.");
+      var useLowercase = window.confirm("Use lower case in your password? OK = YES.");
+      var useNumeric = window.confirm("Use numbers in your password? OK = YES.");
+  }
   while (passwordLength < 8) {
       var passwordLength = window.prompt("Please enter a number between 8 and 128.");
     }
-
+    // check for selected criteria
   if (useUppercase) {
     var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   } else {
     uppercase = "";
   }
-
+  if (useLowercase) {
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  } else {
+    lowercase = "";
+  }
+  if (useNumeric) {
+    var numeric = "0123456789";
+  } else {
+    numeric = "";
+  }
+  // check for special characters
   if (useSpecialCharacter) {
     var specialCharacter = "!@#$%^&*()"
   } else {
     specialCharacter = "";
   }
 
-  var characters = "0123456789abcdefghijklmnopqrstuvwxy";
-  var passCharacters = uppercase + specialCharacter + characters;
+  var passCharacters = uppercase + lowercase + numeric + specialCharacter;
   var password = "";
 
-  for (var i = 0; i <= passwordLength; i++) {
+  for (var i = 0; i < passwordLength; i++) {
     var randomNumber = Math.floor(Math.random() * passCharacters.length);
     password += passCharacters.substring(randomNumber, randomNumber +1);
     console.log(password);
